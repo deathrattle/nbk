2beb25628f9bb479a354fe542c366120e0a0cdc6
Rishabh Teotia <34571994+deathrattle@users.noreply.github.com>
2019-08-27 09:52:56 +0530

    Update nbk.py

--- actions/nbk.py
+++ actions/nbk.py
@@ -2,18 +2,19 @@ import sys
 import json
 import requests
 from st2common.runners.base_action import Action
-class Myclass(Action):
-    def run(self,id,title,desc,pgc,exc,pbp):
-      try:
-            x = {"ID": id, "TITLE": title, "Description": desc, "PageCount":pgc, "Excerpt": exc, "PublishDate": pbp }
-            x1= json.dumps(x)
-            url='https://fakerestapi.azurewebsites.net/api/Books'
-            headers='Content-Type: application/json'
-            r = requests.post(url, headers= headers, data = x1,timeout=6.0)
-            y= r.json()
-            print(r)
-            print(y)
-      except requests.exceptions.Timeout:
-             print("timeout")
-             sys.exit(0)
+class MyAction(Action):
+	def run(self,id,title,desc,pgc,exc,pbp):
+		try:
+			data={"ID": id,"Title":title,"Description": desc,"PageCount": pgc,"Excerpt": exc,  "PublishDate": pbp}
+			data1=json.dumps(data)
+			headers={'content-type': 'application/json'}
+			url='https://fakerestapi.azurewebsites.net/api/Books'
+			response=requests.post(url,headers=headers,data=data1,timeout=6.0)
+			print(response)
+			data2=response.json()
+			print(data2)
+			
+		except requests.exceptions.Timeout:
+                        print("timeout")
+                        sys.exit(0)
     
